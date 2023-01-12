a3b9fe69db7c4987b6452c3e983fc684dbb0ff95
x47 <72315013+x47base@users.noreply.github.com>
2023-01-12 14:27:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Ad Block
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
+// @version      0.2
+// @description  A script which removes ads from datenbanken-verstehen.de
 // @author       https://github.com/x47base
 // @license      MIT
 // @match        https://www.datenbanken-verstehen.de/*
@@ -14,6 +14,7 @@
     'use strict';
 
     // Your code here...
+    // Your code here...
     const ads_datenbanken_de = [
         'ad-block-content-right',
         'ad-block-content-left',
@@ -22,11 +23,14 @@
         'ad-block-homepage-footer',
         'ad-block-homepage-center',
         'ad-block-homepage-right',
+        'ad-block-sticky-ad',
+        'ad-block-text',
+        'ad-block-text-links',
+        'ad-block-text-link',
     ]
     const len = ads_datenbanken_de.length
     const remove = (sel) => document.querySelectorAll(sel).forEach(el => el.remove());
     for (var i = 0; i < len; i++){
         remove(`.${ads_datenbanken_de[i]}`)
     }
-
 })();
