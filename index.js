89717cd88fd29e0bc4c8fad80dbd8fea71c89bc2
x47 <72315013+x47base@users.noreply.github.com>
2023-01-12 14:41:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ad Block
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  A script which removes ads from datenbanken-verstehen.de
 // @author       https://github.com/x47base
 // @license      MIT
@@ -14,38 +14,44 @@
     'use strict';
 
     // Your code here...
-    // Your code here...
-    const ads_datenbanken_de = [
-        'ad-block-content-right',
-        'ad-block-content-left',
-        'ad-block-content-center',
-        'ad-block-homepage-header',
-        'ad-block-homepage-footer',
-        'ad-block-homepage-center',
-        'ad-block-homepage-right',
-        'ad-block-sticky-ad',
-        'ad-block-text',
-        'ad-block-text-links',
-        'ad-block-text-link',
-    ]
-    const len = ads_datenbanken_de.length
-    const remove = (sel) => document.querySelectorAll(sel).forEach(el => el.remove());
-    for (var i = 0; i < len; i++){
-        remove(`.${ads_datenbanken_de[i]}`)
-    }
-    function getImagesByAlt(alt) {
-        var allImages = document.getElementsByTagName("img");
-        var images = [];
-        for (var i = 0, len = allImages.length; i < len; ++i) {
-            if (allImages[i].alt == alt) {
-                images.push(allImages[i]);
-            }
+    function removeAds(){
+        const ads_datenbanken_de = [
+            'ad-block-content-right',
+            'ad-block-content-left',
+            'ad-block-content-center',
+            'ad-block-homepage-header',
+            'ad-block-homepage-footer',
+            'ad-block-homepage-center',
+            'ad-block-homepage-right',
+            'ad-block-sticky-ad',
+            'ad-block-text',
+            'ad-block-text-links',
+            'ad-block-text-link',
+        ]
+        const len = ads_datenbanken_de.length
+        const remove = (sel) => document.querySelectorAll(sel).forEach(el => el.remove());
+        for (var i = 0; i < len; i++){
+            remove(`.${ads_datenbanken_de[i]}`)
         }
-        return images;
+        function getImagesByAlt(alt) {
+            var allImages = document.getElementsByTagName("img");
+            var images = [];
+            for (var i = 0, len = allImages.length; i < len; ++i) {
+                if (allImages[i].alt == alt) {
+                    images.push(allImages[i]);
+                }
+            }
+            return images;
+        }
+        const AdImages = getImagesByAlt("autopromo");
+        const len2 = AdImages.length
+        for (var v = 0; v < len2; v++){
+            getImageByAlt[v].parentNode.removeChild(getImageByAlt[v]);
+        }
     }
-    const AdImages = getImagesByAlt("autopromo");
-    const len2 = AdImages.length
-    for (var v = 0; v < len2; v++){
-        getImageByAlt[v].parentNode.removeChild(getImageByAlt[v]);
-    }
+    // Routine Deployment
+    removeAds()
+    setTimeout(function () {
+        removeAds()
+    }, 5000);
 })();
